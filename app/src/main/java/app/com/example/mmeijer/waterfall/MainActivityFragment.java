e1ee40941f50fcbccbf365be039534b518fd4576
McMm77 <mmeijer@29CT9T1.nsn-intra.net>
2016-03-04 17:20:33 +0100

    Lesson1
    Lesson 1 completed

--- app/src/main/java/app/com/example/mmeijer/waterfall/MainActivityFragment.java
+++ app/src/main/java/app/com/example/mmeijer/waterfall/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package app.com.example.mmeijer.waterfall;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] forecastArray = {
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63",
+          "Today - Sunny - 88/63"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
