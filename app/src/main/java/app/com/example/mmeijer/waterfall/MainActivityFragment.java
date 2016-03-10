b7e1b6f54e370c8918b33b5f2b7c5af8c84d47a9
McMm77 <maciej.meijer@nokia.com>
2016-03-10 23:30:42 +0100

    DetailActivity
    DetailActivity does not show settings menu. Probably due to wrong extend
    class

--- app/src/main/java/app/com/example/mmeijer/waterfall/MainActivityFragment.java
+++ app/src/main/java/app/com/example/mmeijer/waterfall/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package app.com.example.mmeijer.waterfall;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -89,7 +92,16 @@ public class MainActivityFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent forecastIntent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(forecastIntent);
+            }
+        });
 
         return rootView;
     }
